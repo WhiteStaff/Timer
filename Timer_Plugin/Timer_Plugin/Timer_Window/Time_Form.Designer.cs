232abc57cb9685959b4aae0b6e4c443998542a90
Unknown <sashakamensk@mail.ru>
2018-12-09 00:13:40 +0300

    Working Time

--- Timer_Plugin/Timer_Plugin/Timer_Window/Time_Form.Designer.cs
+++ Timer_Plugin/Timer_Plugin/Timer_Window/Time_Form.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 130);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "";
+            
+            // 
+            // Time_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.HelpButton = true;
+            this.Name = "Time_Form";
             this.Text = "Time_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
---
+++ Timer_Plugin/Timer_Plugin/Timer_Window/Time_Form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Timer_Plugin
+{
+    public partial class Time_Form : Form
+    {
+        public Time_Form()
+        {
+
+
+            
+
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 50;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(timer_Tick);
+
+            //label1.Text = DateTime.Now.ToString("HH:mm:ss");
+
+
+        }
+
+        /*private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            (DateTime.Now.ToString("HH:mm:ss"));
+           
+        }*/
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //InitializeComponent();
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            label1.Update();
+        }
+    }
+}
